3d761723e97d1ed72a0c44492b9dac8631dbd3e9
William Crews <crewswh@gmail.com>
2021-08-03 19:40:47 -0400

    new things

--- script.js
+++ script.js
@@ -2,17 +2,36 @@
 // var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  // passwordText.value = password;
+//   passwordText.value = password;
 
-  console.log("connected")
-
-}
+// }
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
 
 
+function writePassword() {
+  console.log("connected");
+  
+  var passwordText = document.getElementById("#password");
+  var lets = "abcdefghijklmnopqrstuvwxyz";
+  var numbs = "0123456789"
+  var puncts = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  console.log(abc);
+  
+
+
+
+}
+
+
+// const copyBtn = document.getElementById("copy");
+// copyBtn.addEventListener("click", () => {
+//   passwordTxt.select();
+//   document.execCommand("copy");
+//   alert("Password Copied");
+// });
